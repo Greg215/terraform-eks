ab6f38afe38ecd20e67fdf67649301eda3af7601
Greg Hu <guangyue.hu@sumup.com>
2021-09-06 04:21:43 +0200

    update version

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-devops-training-bucket"
-    key    = "example/terraform.tfstate"
+    key    = "circleci-demo-greg/terraform.tfstate"
     region = "ap-southeast-1"
   }
   required_version = ">= 0.12.0"
